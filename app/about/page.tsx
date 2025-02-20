0355fe31812b25a5df5fff306fd012befbfb1cb9
VanshajTiwari <vanshajtiwari62@gmail.com>
2025-02-20 16:42:34 +0530

    fix errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ return <>
             <section className="bg-gray-200 text-green-900 md:p-10 flex flex-col items-center" style={{fontFamily:"'Quicksand'"}}>
                 <h1 className="text-6xl">EXPERIENCE  BRILLIANCE</h1>
                 <p className="text-center md:p-10">
-                    Small team. Big moments. A lifetime of happiness. With more than twelve years of experience planning wedding and events, we're experts in perfecting the details and creating the extraordinary. We exist so that you can enjoy the 
+                    Small team. Big moments. A lifetime of happiness. With more than twelve years of experience planning wedding and events, we&apos;re experts in perfecting the details and creating the extraordinary. We exist so that you can enjoy the 
                     fun parts of planning and leave the stress behind. Our clients are intensely busy professionals who depend on us to make their planning process stress-free and to discover what will make their day uniquely special. We make 
                     design and planning simple for our clients - and simple isn't easy.
                     Born at the intersection of clever and creative, Brilliant Event Planning harnesses the power of emotion to make your most important day less stressful and more beautiful. At our core, we are a culture of action and imagination.
